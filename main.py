433072fa1baf23d12e4bbbde93c22c4218491b63
i5-PC <mcienkowski@gmail.com>
2023-08-28 15:43:49 +0200

    Add clear button and exit button, theme was changed, clearing functionality added to clear_button

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import PySimpleGUI as sg
 
+sg.theme("DarkBlue15")
+def clear_inputs():
+    window["num1"].update("")
+    window["num2"].update("")
+    window["symbol"].update("")
+    window["result"].update("")
+
 label = sg.Text('Enter Numbers')
 user_num1 = sg.Input(key="num1", size=(10,5), justification="c")
 user_num2 = sg.Input(key="num2", size=(10,5), justification="c")
 user_symbol = sg.Input(key="symbol", size=(2,2), justification="c")
 output = sg.Text("", key="result")
+clear_button = sg.Button("Clear", key="clear")
 calc_button = sg.Button("Calculate",key="calc")
 exit_button = sg.Button("Exit", key="exit")
 
@@ -12,7 +20,7 @@ exit_button = sg.Button("Exit", key="exit")
 
 layout = [[label],
           [user_num1, user_symbol, user_num2, output],
-          [calc_button]]
+          [clear_button, calc_button, exit_button]]
 
 
 window = sg.Window("Calculator", layout=layout,
@@ -25,7 +33,23 @@ while True:
             break
         case sg.WIN_CLOSED:
             break
-
+        case "clear":
+            clear_inputs()
+    try:
+        num1 = float(values["num1"])
+        num2 = float(values["num2"])
+        sym = values["symbol"]
+        if (sym == "+"):
+            result = num1 + num2
+        elif (sym == "-"):
+            result = num1 - num2
+        elif (sym == "*"):
+            result = num1 * num2
+        elif (sym == "/"):
+            result = num1 / num2
+        window["result"].update(result)
+    except ValueError:
+        sg.popup("Enter numbers only!", font=("Helvetica", 10))
 
 
 
